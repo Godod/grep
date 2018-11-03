8a8a13afc31839fb140b6b90bc86116ca00bc786
Denys T <titan074@gmail.com>
2018-11-03 18:55:29 +0200

    Add exceptions, add Validator, add docstrings and type hints

--- grep.py
+++ grep.py
@@ -1,40 +1,110 @@
+import os.path
 import re
 import argparse
+import mimetypes
+from typing import Generator, Optional
+
+# Base directory path
+BASE_DIR = os.path.dirname(__file__)
+# Allowed file mimetype
+FILE_MIMETYPE = 'text/plain'
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Grep utility on Python')
-    parser.add_argument('search', help='Text to find in file. Can be a regex.')
-    parser.add_argument('filepath', help='Path to file')
-    parser.add_argument('-v', action='store_true')
-    args = parser.parse_args()
+class GrepException(Exception):
+    pass
 
-    search = args.search
-    filepath = args.filepath
-    not_included = args.v
 
-    searcher = Searcher(search, filepath)
-    for line in searcher.find(not_included):
-        if line:
-            print(line)
+class ValidationError(GrepException):
+    pass
+
+
+class Validator:
+    """Class for validate input data."""
+
+    def __init__(self, filepath: str):
+        """Constructor.
+
+        Args:
+            filepath (str): full path to file.
+        """
+        self.filepath = filepath
+
+    def validate_path(self):
+        """Check if file exist and have the right mimetype.
+
+        Raises:
+            :py:obj:`ValidationError`: Error if file is not exist or haven't
+                wrong mimetype.
+        """
+        if not os.path.isfile(self.filepath):
+            raise ValidationError(f'{self.filepath} is not a file.')
+
+        mimetype, _ = mimetypes.guess_type(self.filepath)
+        if mimetype is None or mimetype != FILE_MIMETYPE:
+            msg = f'{self.filepath} has wrong mimetype {mimetype}. '\
+                'File must be a {FILE_MIMETYPE} type.'
+            raise ValidationError(msg)
 
 
 class Searcher:
+    """Class for search text in a file by it's path."""
 
-    def __init__(self, search: str, filepath: str):
-        self.search = re.compile(search)
+    def __init__(self, text: str, filepath: str):
+        """Constructor.
+
+        Args:
+            text (str): text to search. Can be a regex expression.
+            filepath (str): full path to file.
+        """
+        self.text = re.compile(text)
         self.file = filepath
 
-    def find(self, not_include: bool = False):
+    def find(self, not_include: bool = False) -> Generator[str, None, None]:
+        """Find a line in a file.
+
+        Args:
+            not_include (bool): If True find a line that not contain a string.
+
+        Returns:
+            generator: return a search line.
+        """
         with open(self.file, 'r') as f:
             for line in f:
-                result = self._processing(line)
+                result = self._search(line)
 
                 if (result is None and not_include) or result is not None:
                     yield line
 
-    def _processing(self, line: str):
-        return self.search.search(line)
+    def _search(self, line: str) -> re.Match:
+        """Search a precompilated string in a text line.
+
+        Args:
+            line (str): line to search.
+
+        Returns:
+            re.Match: a corresponding match object.
+        """
+        return self.text.search(line)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Grep utility on Python')
+    parser.add_argument('text', help='Text to find in file. Can be a regex.')
+    parser.add_argument('filepath', help='Path to file')
+    parser.add_argument('-v', action='store_true')
+    args = parser.parse_args()
+
+    text = args.text
+    filepath = os.path.join(BASE_DIR, args.filepath)
+    not_included = args.v
+
+    validator = Validator(filepath)
+    validator.validate_path()
+
+    searcher = Searcher(text, filepath)
+    for line in searcher.find(not_included):
+        if line:
+            print(line)
 
 
 if __name__ == '__main__':
